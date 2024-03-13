dadddc3c4bd489ce2db1f4bbff6c0e4f9476de65
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-03-13 13:01:59 -0700

    Create second export

--- my-first-react-app/src/Greeting.jsx
+++ my-first-react-app/src/Greeting.jsx
@@ -2,4 +2,13 @@ function Greeting() {
     return <h1>&quot;Just out for a rip are ya, bud?&quot;</h1>
 }
 
-export default Greeting;
+
+function Greeting2() {
+    return <h2>Just out for a rip</h2>
+}
+
+
+export {
+    Greeting,
+    Greeting2
+}
