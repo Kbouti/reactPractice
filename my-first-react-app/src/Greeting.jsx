307193b34aad2600c9bf38d20d52357f5fb3733a
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-03-14 19:18:49 -0700

    Replace canadian greeting with animals list from lesson

--- my-first-react-app/src/Greeting.jsx
+++ my-first-react-app/src/Greeting.jsx
@@ -1,14 +1,37 @@
 function Greeting() {
-    return <h1>&quot;Just out for a rip are ya, bud?&quot;</h1>
+  return <h1>&quot;Just out for a rip are ya, bud?&quot;</h1>;
 }
-
 
 function Greeting2() {
-    return <h2>Just out for a rip</h2>
+  return <h2>Just out for a rip</h2>;
+}
+
+function ListItem(props) {
+  return <li>{props.animal}</li>;
+}
+
+function List(props) {
+  return (
+    <ul>
+      {props.animals.map((animal) => {
+        return <ListItem key={animal} animal={animal} />;
+      })}
+    </ul>
+  );
 }
 
 
-export {
-    Greeting,
-    Greeting2
-}
+
+function App2() {
+    const animals = ["Lion", "Cow", "Snake", "Lizard"];
+  
+    return (
+      <div>
+        <h1>Animals: </h1>
+        <List animals={animals} />
+      </div>
+    );
+  }
+
+
+export { Greeting, Greeting2, App2 };
--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import {Greeting, Greeting2 } from './Greeting.jsx'
+// import App from './App.jsx'
+import {Greeting, Greeting2, App2} from './Greeting.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Greeting />
-    <Greeting2/>
+    <App2/>
   </React.StrictMode>,
 )
