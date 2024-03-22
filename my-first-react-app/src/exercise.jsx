3c815a1e3478112072af50fba221e79e07512ba6
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-03-22 12:20:18 -0700

    Log state values at various points

--- my-first-react-app/src/exercise.jsx
+++ my-first-react-app/src/exercise.jsx
@@ -2,21 +2,20 @@ import { useState } from "react";
 
 function Person() {
     const [person, setPerson] = useState({name: "John", age: 100 });
-
     const handleIncreaseAge = () => {
-
-        const newPerson = {...person, age: person.age + 1 };
             // This^^ is super helpful syntax for: Copy this object, but change this one thing
-            setPerson(newPerson);
+            console.log(`in handleIncreaseAge (before setPerson call:`, person);
+            setPerson({...person, age: person.age + 1 });
+            console.log(`in handleIncreaseAge (after setPerson call:`, person);
     }
 
-
+console.log(`during render:`, person);
 
 return (
     <>
     <h1>{person.name}</h1>
     <h2>{person.age}</h2>
-    <button onclick={handleIncreaseAge}>Increase age</button>
+    <button onClick={handleIncreaseAge}>Increase age</button>
     </>
 )
 
